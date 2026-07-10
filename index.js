b4e4af50869137ae18841929ed66dad165e5d4d1
Tiago Pereira <tiagonet50@gmail.com>
2026-07-10 10:28:22 -0300

    package.json

--- index.js
+++ index.js
@@ -8,19 +8,6 @@ import os from 'os'
 import path from 'path'
 import { FilenSDK } from '@filen/sdk'
 
-/*
-const app = express()
-const PORT = process.env.PORT || 8000
-
-app.get('/', (req, res) => {
-    res.send('Running the app!')
-})
-
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-})
-*/
-
 const filen = new FilenSDK({
     metadataCache: true,
     connectToSocket: true,
